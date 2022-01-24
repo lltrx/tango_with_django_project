c4a00277c13bee911ea25548481b6222163ec782
turki faisal <turki@turkis-MacBook-Air.local>
2022-01-24 15:28:28 +0300

    Edit on chapter 3, and the test is OK

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,7 +16,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
@@ -120,12 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#Media files
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
