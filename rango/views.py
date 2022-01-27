90abe0e8c3e8e4631a4f746c0123f2863b971359
turki faisal <turki@turkis-MacBook-Air.local>
2022-01-28 01:37:30 +0300

    Finshed Chapter 7, and the test is OK

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
+
 
 def index(request):
 
@@ -9,19 +14,19 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
-    context_dict['boldmessage'] ='Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
     return render(request, 'rango/index.html', context=context_dict)
-    
+
 
 def about(request):
 
-
-    context_dict = {'boldmessage': 'This tutorial has been put together by Turki Faisal '}
+    context_dict = {
+        'boldmessage': 'This tutorial has been put together by Turki Faisal '}
     return render(request, 'rango/about.html', context=context_dict)
-    
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -37,5 +42,45 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
 
+    return render(request, 'rango/category.html', context=context_dict)
 
-    return render(request, 'rango/category.html' , context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+        return redirect('/rango/')
+    else:
+        print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+        # You cannot add a page to a Category that does not exist...
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return redirect(reverse('rango:show_category',
+                                        kwargs={'category_name_slug':
+                                                category_name_slug}))
+        else:
+            print(form.errors)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
